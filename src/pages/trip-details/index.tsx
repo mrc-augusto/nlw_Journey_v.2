6a6a5e961dfdddacfde6de5a229dfe0d27242e9c
Maurício Augusto <mrc_augusto@hotmail.com>
2025-01-29 12:56:26 -0300

    Trip Details Page Main style created

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage(){
   return(
@@ -22,6 +22,58 @@ export function TripDetailsPage(){
           </button>
         </div>
       </div>
+
+      <main className="flex gap-16 px-4">
+        <div className="flex-1 space-y-6">
+
+          {/* Header */}
+          <div className="flex items-center justify-between">
+            <h2 className="text-zinc-50 font-semibold text-3xl">Atividades</h2>
+            <button className="bg-lime-300 flex items-center gap-2 py-2 px-5 font-medium text-lime-950 rounded-lg hover:bg-lime-400">
+              <Plus className="size-5"/>
+              Cadastrar atividade
+            </button>
+          </div>
+
+          <div className="space-y-8">
+            <div className="space-y-3">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-zinc-300 text-xl font-semibold">Dia 17</span>
+                <span className="text-zinc-500 text-xs">Sábado</span>
+              </div>
+              <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data</p>
+            </div>
+            <div className="space-y-3">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-zinc-300 text-xl font-semibold">Dia 18</span>
+                <span className="text-zinc-500 text-xs">Domingo</span>
+              </div>
+              <div className="space-y-3">
+                <div className="bg-zinc-900 flex gap-3 justify-between items-center px-4 py-2.5 shadow-shape rounded-xl">
+                  <CircleCheck className="size-5 text-lime-300"/>
+                  <span className="flex-1 text-zinc-100">Corrida de Kart</span>
+                  <span className="text-zinc-400 text-sm">14:00h</span>
+                </div>
+              </div>
+              <div className="space-y-3">
+                <div className="bg-zinc-900 flex gap-3 justify-between items-center px-4 py-2.5 shadow-shape rounded-xl">
+                  <CircleCheck className="size-5 text-lime-300"/>
+                  <span className="flex-1 text-zinc-100">Corrida de Kart</span>
+                  <span className="text-zinc-400 text-sm">14:00h</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="w-80">
+          <div>
+            <p className="text-zinc-50">Links importantes</p>
+
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
